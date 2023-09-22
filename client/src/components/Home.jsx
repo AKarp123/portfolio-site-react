1497a0347c595e99f0fa01dc4477d42b8117f819
Akarp123 <ash.daboss111@gmail.com>
2023-09-21 21:08:18 -0400

    cursor to website

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect } from "react";
-import { Grid, Typography, Link as MUILink } from "@mui/material";
+import { Grid, Typography, Link as MUILink, Collapse, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import "../Home.css";
 import Github from "../imgs/github-icon.webp";
 
 const homeLinkStyles = () => ({
-    fontSize: "1.8em",
+    fontSize: "1.4em",
 });
 
 export default function Home() {
-    const [hover, setHover] = React.useState(false);
+    const [clicked, setClicked] = React.useState(false);
     const [loaded, setLoaded] = React.useState(false);
 
     useEffect(() => {
@@ -22,9 +22,11 @@ export default function Home() {
             transition: "opacity 1s ease-in-out",
         };
     };
+    
 
-    const handleMouseHover = () => {
-        setHover(!hover);
+    const onClick = () => {
+        console.log("clicked");
+        setClicked(!clicked);
     };
 
     return (
@@ -39,43 +41,56 @@ export default function Home() {
                     item
                     lg={2}
                     xs={12}
-                    sx={{ alignItems: "flex-start", marginTop: "35vh" }}
+                    sx={{
+                        alignItems: "flex-start",
+                        marginTop: "35vh",
+                        justifyContent: "center",
+                    }}
                 >
-                    <div
-                        onMouseEnter={handleMouseHover}
-                        onMouseLeave={handleMouseHover}
-                    >
+                    <div onClick={onClick} style={{cursor: "pointer"}}>
                         <h1 className="Home-Name">Ashton Karp</h1>
                     </div>
                 </Grid>
-                <Grid item lg={2} xs={12} sx={{ marginTop: "35vh" }}>
-                    <Typography
-                        as={Link}
-                        to="/about"
-                        className="Home-Link"
-                        sx={homeLinkStyles()}
-                    >
-                        About Me
-                    </Typography>
-                    <Typography
-                        as={Link}
-                        to="/projects"
-                        className="Home-Link"
-                        sx={homeLinkStyles()}
-                    >
-                        Projects
-                    </Typography>
-                    <Typography
-                        as={MUILink}
-                        href="https://github.com/AKarp123/"
-                        target="_blank"
-                        className="Home-Link"
-                        sx={{...homeLinkStyles(), textDecoration: "none", color: "white"}}
-                    >
-                        GitHub
-                    </Typography>
-                </Grid>
             </Grid>
+            <Collapse in={clicked}>
+                <Grid container spacing={3} justifyContent="center">
+                    <Grid item lg={1} xs={12}>
+                        <Typography
+                            as={Link}
+                            to="/about"
+                            className="Home-Link"
+                            sx={homeLinkStyles()}
+                        >
+                            About Me
+                        </Typography>
+                    </Grid>
+                    <Grid item lg={1} xs={12}>
+                        <Typography
+                            as={Link}
+                            to="/projects"
+                            className="Home-Link"
+                            sx={homeLinkStyles()}
+                        >
+                            Projects
+                        </Typography>
+                    </Grid>
+                    <Grid item lg={1} xs={12}>
+                        <Typography
+                            as={MUILink}
+                            href="https://github.com/AKarp123/"
+                            target="_blank"
+                            className="Home-Link"
+                            sx={{
+                                ...homeLinkStyles(),
+                                textDecoration: "none",
+                                color: "white",
+                            }}
+                        >
+                            GitHub
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Collapse>
         </>
     );
 }
